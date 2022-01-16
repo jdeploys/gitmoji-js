a88b2f85abc89d04cc6b52ee5bc38ed0170badfe
Juseong Kim <dcjuja@gmail.com>
2022-01-16 15:01:00 +0900

    🔍️ 임시 라우터 삭제

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,11 +9,11 @@ const routes: RouteRecordRaw[] = [
 
   // Always leave this as last one,
   // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    // 필요한 경우 여기에 404 페이지 작성
-    component: () => import('layouts/MainLayout.vue'),
-  },
+  // {
+  //   path: '/:catchAll(.*)*',
+  //   // 필요한 경우 여기에 404 페이지 작성
+  //   component: () => import('layouts/MainLayout.vue'),
+  // },
 ];
 
 export default routes;
